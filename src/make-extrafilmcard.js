dff99afed4eebf688482069fb126bd52380a7d50
scream941 <scream941@mail.ru>
2019-03-05 23:16:14 +0300

    добавление функции формирующей структуру данных для карточки с фильмом и последующая отрисовка шаблонов использую полученные данные

--- src/make-extrafilmcard.js
+++ src/make-extrafilmcard.js
@@ -1,13 +1,15 @@
-export default () => `<article class="film-card film-card--no-controls">
-  <h3 class="film-card__title">Incredibles 2</h3>
-  <p class="film-card__rating">9.8</p>
+import random from './random.js';
+
+export default (film) => `<article class="film-card film-card--no-controls">
+  <h3 class="film-card__title">${film.title}</h3>
+  <p class="film-card__rating">${film.rating}</p>
   <p class="film-card__info">
-    <span class="film-card__year">2018</span>
-    <span class="film-card__duration">1h&nbsp;13m</span>
-    <span class="film-card__genre">Comedy</span>
+    <span class="film-card__year">${film.year}</span>
+    <span class="film-card__duration">${film.duration}</span>
+    <span class="film-card__genre">${[...film.genre][random(6)]}</span>
   </p>
 
-  <img src="./images/posters/accused.jpg" alt="Accused" class="film-card__poster">
+  <img src="${film.poster}" alt="Accused" class="film-card__poster">
 
-  <button class="film-card__comments">13 comments</button>
+  <button class="film-card__comments">${film.amountOfComments} comments</button>
 </article>`;
