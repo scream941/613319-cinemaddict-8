f18c1cc7920a1496f91b8b1e7ec4d53f885d427c
scream941 <scream941@mail.ru>
2019-04-02 01:04:51 +0300

    добавление классов для карточек фильмов, попапа а так же работа с событиями на этих элементах

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import makeFilter from './make-filter.js';
-import makeMainCard from './make-filmcard.js';
-import makeExtraCard from './make-extrafilmcard.js';
 import getFilm from './filmData.js';
-import random from './random.js';
+import {random, simpleCicle} from './utilites.js';
+import FilmCard from './FilmCard.js';
+import FilmExtraCard from './FilmExtraCard.js';
+import Popup from './Popup.js';
 
 const filterContainer = document.querySelector(`.main-navigation`);
 const filmsList = document.querySelector(`.films-list`);
@@ -12,11 +13,39 @@ const filmsMostComment = document.querySelector(`section:nth-of-type(3) .films-l
 let isOpened = false;
 
 
-const renderFilmCards = (dist, count, mapper) => {
-  dist.innerHTML = ``;
-  const cards = new Array(count).fill(``).map(() => mapper(getFilm()));
-  dist.insertAdjacentHTML(`beforeend`, cards.join(``));
+filmsMainContainer.innerHTML = ``;
+filmsTopRated.innerHTML = ``;
+filmsMostComment.innerHTML = ``;
+
+
+const listOfMainFilms = [];
+const listOfTopRatedFilms = [];
+const listOfMostCommentFilms = [];
+simpleCicle(listOfMainFilms, 7, getFilm);
+simpleCicle(listOfTopRatedFilms, 2, getFilm);
+simpleCicle(listOfMostCommentFilms, 3, getFilm);
+
+
+const renderFilms = (dist, listOfFilms) => {
+  for (const film of listOfFilms) {
+    const filmCard = new FilmCard(film);
+    filmCard.onClick = () => {
+      document.body.appendChild(popup.render());
+    };
+    const popup = new Popup(film);
+    popup.onClose = () => {
+      popup._element.remove();
+      popup.unrender();
+    };
+    dist.appendChild(filmCard.render());
+  }
 };
+
+
+renderFilms(filmsMainContainer, listOfMainFilms);
+renderFilms(filmsTopRated, listOfTopRatedFilms);
+renderFilms(filmsMostComment, listOfMostCommentFilms);
+
 
 function open(e) {
   const opened = filterContainer.querySelector(`.main-navigation__item--active`);
@@ -43,8 +72,5 @@ filterContainer.insertAdjacentHTML(`afterbegin`, makeFilter(`Favorites`, true, 4
 filterContainer.insertAdjacentHTML(`afterbegin`, makeFilter(`Watchlist`, true, 10));
 filterContainer.insertAdjacentHTML(`afterbegin`, makeFilter(`All`));
 
-renderFilmCards(filmsMainContainer, 7, makeMainCard);
-renderFilmCards(filmsTopRated, 2, makeExtraCard);
-renderFilmCards(filmsMostComment, 2, makeExtraCard);
 
 filterContainer.addEventListener(`click`, open);
--- src/random.js
+++
@@ -1,3 +0,0 @@
-const random = (max, min = 0) => Math.floor(Math.random() * (max - min) + min);
-
-export {random};
